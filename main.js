10dc46fac6efd8f3a61251b9aae12c81a91b64ad
Artturi Kakriainen <artturi.kakriainen@edu.turkuamk.fi>
2022-08-07 20:15:27 +0300

    tasklist

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ class Task {
     }
 }
 
-function triggerTest() {
-    fetch('https://pilvitask.azurewebsites.net/api/HttpTrigger1?code=BVC3Nck6PmrJtDFbC-LXeGlxMLTPPK3gA0J9MDSJVpUtAzFujWu5WA==')
+async function triggerTest() {
+    await fetch('https://pilvitask.azurewebsites.net/api/HttpTrigger1?code=BVC3Nck6PmrJtDFbC-LXeGlxMLTPPK3gA0J9MDSJVpUtAzFujWu5WA==')
         .then((response) => response.json())
         .then((data) => taskList.push(data));
     console.log(taskList)
