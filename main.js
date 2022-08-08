a86c243ee0d3e38a1ad5ee7a5d08c81941de5287
Artturi Kakriainen <artturi.kakriainen@edu.turkuamk.fi>
2022-08-08 19:52:19 +0300

    delete fi

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
 var counter = 0
 
 var taskList = []
+const btn = document.getElementById('trigbtn')
+const taskDescr = document.getElementById('taskname')
+const taskView = document.querySelector('ul')
+
 
 
 class Task {
-    constructor(id, label, descr) {
+    constructor(id, descr) {
         this.id = id + Date.now()
         this.descr = descr
     }
 }
 
+
+
 async function triggerTest() {
-    document.getElementById('trigbtn').disabled = true;
+    btn.disabled = true;
+
     await fetch('https://pilvitask.azurewebsites.net/api/HttpTrigger1?code=BVC3Nck6PmrJtDFbC-LXeGlxMLTPPK3gA0J9MDSJVpUtAzFujWu5WA==')
         .then((response) => response.json())
         .then((data) => taskList.push(data));
-    document.getElementById('trigbtn').disabled = false;
-    console.log(taskList)
+
+    updateView()
+    btn.disabled = false;
+}
+
+
+
+function updateView() {
+    taskView.innerHTML = null
+    taskList.forEach(task => taskToView(task))
+}
+
+
+
+function taskToView(task) {
+    let entry = document.createElement('div')
+    entry.id = "entry_" + task.id
+    entry.style.minHeight = '40px'
+    entry.appendChild(generateDescr("descr" + task.id, task.descr))
+    entry.appendChild(generateBtn(task.id, "Del", entry.id))
+    taskView.appendChild(entry)
+}
+
+
+
+function taskToList() {
+    let taskInput = document.getElementById("taskname")
+    if (taskInput.value != "") {
+        const localTask = new Task("task" + Date.now(), taskDescr.value)
+        taskList.push(localTask)
+        taskInput.value = null
+        updateView()
+    }
+    else {
+        alert("No task!")
+    }
 }
 
 
@@ -26,10 +67,10 @@ function generateBtn(_id, content, entryId) {
     let btnDiv = document.createElement('div')
     let btn = document.createElement("button")
 
-    btn.id = _id
+    btn.id = "btn" + _id
     btn.innerHTML = content
     btn.onclick = function () {
-        delItem(entryId);
+        delItem(_id, entryId);
     }
     btnDiv.appendChild(btn)
     btnDiv.style.float = 'right'
@@ -52,41 +93,9 @@ function generateDescr(_id, content) {
 
 
 
-function delItem(_id) {
-    console.log("delete " + _id)
-    document.getElementById(_id).remove()
+function delItem(taskId, entryId) {
+    taskList = taskList.filter(task => task.id !== taskId)
+    document.getElementById(entryId).remove()
 }
 
 
-
-function addTask(local = true) {
-
-    if (local != true) {
-
-    }
-    else {
-        const task = document.querySelector("input")
-
-        if (task.value.length != 0) {
-
-            let list = document.querySelector("ul")
-
-            let entry = document.createElement('div')
-
-            entry.classList.add("taskitem")
-            entry.id = "task" + counter;
-            entry.style.minHeight = '40px'
-            entry.appendChild(generateDescr("descr" + counter, task.value))
-            entry.appendChild(generateBtn("delBtn" + counter, "del", entry.id))
-
-            list.appendChild(entry)
-
-            counter++
-
-            task.value = null
-        }
-        else {
-            alert("No Task Specified!")
-        }
-    }
-}
